0c6eafd211e3e32766d810985bdef22be5b420c6
Lingzi <lingziii@outlook.com>
2026-01-04 16:51:08 +0100

    Fix FAB visiblity issues. Try to fix scrolling up bg elements.

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const overlay = document.getElementById('overlay')
 
 openBottomSheet.addEventListener("click", () => {
     overlay.style.display = 'flex';
+    openBottomSheet.style.display = 'none';
 })
 
 overlay.addEventListener('click', (event) => {
     if (event.target === overlay) {
     overlay.style.display = 'none';
+    openBottomSheet.style.display = 'block';
 }})
